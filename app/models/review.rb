25b9f1c693017c67b9cc93fd196cb842672dc76f
Tyler <toygrrr@gmail.com>
2022-09-06 15:56:52 -0700

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
-  
+#   def game
+#     Game.find(self.game_id)
+#   end
+
+# this is a macro built in to do the above method for us, we also have a macro called has_many
+# game is the argument for the belongs_to method.
+# argument MUST be singular and not plural (games)
+belongs_to :game
 end
